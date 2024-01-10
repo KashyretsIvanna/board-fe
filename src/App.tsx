7aa28962616ed7ff1318da9f286b6668468a14f8
Kashyrets Ivanna <ivanna.kashyrets@ex2.team>
2024-01-10 03:53:24 +0200

    fix: small fix

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,8 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/board" element={<UserList />} />
-        <Route path="*" element={<Navigate to={'/board'} />} />
+        <Route path="/" element={<UserList />} />
+        <Route path="*" element={<Navigate to={'/'} />} />
       </Routes>
     </div>
   );
